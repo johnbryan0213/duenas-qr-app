5c214db8448c90dac4e51f5bac15a70fa17c62d0
ohio080500 <mileslaurencejubinal@yahoo.com>
2025-06-20 17:49:46 +0800

    added login page

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,6 +106,16 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-scanner',
+          title: 'Scanner',
+          to: '/scanner'
+        },
+        {
+          icon: 'mdi-login',
+          title: 'Sign in',
+          to: '/signin'
         }
       ],
       miniVariant: false,
---
+++ pages/auth/signin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="d-flex align-center justify-center" style="height: 100vh;">
+        <v-card width="500" class="pa-8 text-center">
+            <h1 class="my-4">Login Page</h1>
+            <v-form>
+                <v-text-field label="Email" outlined prepend-inner-icon="mdi-email"></v-text-field>
+                <v-text-field label="Password" outlined prepend-inner-icon="mdi-lock"></v-text-field>
+                <v-btn color="primary" block>Login</v-btn>
+            </v-form>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    layout: "auth"
+}
+</script>
