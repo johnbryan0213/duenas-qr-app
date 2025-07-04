b1fd2edb98e5bfed3d760fc4ab141a65d11afa6c
ohio080500 <mileslaurencejubinal@yahoo.com>
2025-07-04 18:11:20 +0800

    Added nuxt auth plugins

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="d-flex align-center justify-center" style="height: 100vh;">
-        <v-card width="500" class="pa-8 text-center">
-            <h1 class="my-4">Login Page</h1>
+        <v-card width="400" class="card-outline px-8 py-10 text-center">
+            <h2 class="text-center my-6 hr-sect mb-8">Login Page</h2>
             <v-form>
-                <v-text-field label="Email" outlined prepend-inner-icon="mdi-email"></v-text-field>
-                <v-text-field label="Password" outlined type="password" prepend-inner-icon="mdi-lock"></v-text-field>
-                <v-btn color="primary" block>Login</v-btn>
+                <v-text-field
+                 label="Username" 
+                 outlined 
+                 rounded
+                 prepend-inner-icon="mdi-account"
+                ></v-text-field>
+                <v-text-field 
+                label="Password"
+                rounded
+                outlined type="password" 
+                prepend-inner-icon="mdi-lock"
+                ></v-text-field>
+
+                <v-btn color="primary" class="text capitalize" rounded block>Login</v-btn>
+                <div class="hr-sect my-5">OR</div>
+                <v-btn color="error" class="text-capitalize" rounded block><v-icon left>mdi-google</v-icon> Sign in with google</v-btn>
             </v-form>
         </v-card>
     </div>
@@ -15,3 +28,26 @@ export default {
     layout: "auth"
 }
 </script>
+<style scoped>
+.hr-sect{
+    display: flex;
+    flex-basis: 100%;
+    align-items: center;
+    /* color: rgba(0, 0, 0, 0.9); */
+    margin: 8px 0;
+}
+
+.hr-sect::before,
+.hr-sect::after {
+    content: "";
+    flex-grow: 1;
+    background: rgba(255, 255, 255, 0.15);
+    height: 1px;
+    font-size: 0;
+    line-height: 0;
+    margin: 0 8px;
+}
+.card-outline {
+    border-radius: 20px;
+}
+</style>
